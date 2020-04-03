94989d3984c458f856079ddd8f903cd0bf306c43
ishan <agishandissanayake@gmail.com>
2020-04-03 16:58:44 +0530

    Home Templated **Updated.

--- src/home/pages/home.js
+++ src/home/pages/home.js
@@ -3,65 +3,17 @@ import ImageSlider from "../components/ImageSlider";
 import model1 from "../../shared/assets/tempImages/shop_model_1.png";
 import model2 from "../../shared/assets/tempImages/shop_model_2.png";
 import model3 from "../../shared/assets/tempImages/shop_model_3.png";
+import NewArrivals from "../components/NewArrivals";
 
 const home = () => {
   return (
     <div>
         <div style={{marginTop:'-15px'}}>
         <ImageSlider />
+        <NewArrivals />
         </div>
       
       <hr />
-
-        <div className="container-fluid" id="#section1">
-            <h1 className="text-center">New Arrivals</h1>
-        </div>
-<br /><br />
-        <div className="d-flex justify-content-around">
-            <div className="row">
-                <div className="card m-2" style={{width: '18rem'}}>
-                    <img className="card-img-top" src={model1} alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                            <a href="#" className="btn btn-warning text-white">Buy</a>
-                        </div>
-                </div>
-                <div className="card m-2" style={{width: '18rem'}}>
-                    <img className="card-img-top" src={model2} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
-                        <a href="#" className="btn btn-warning text-white">Buy</a>
-                    </div>
-                </div>
-                <div className="card m-2" style={{width: '18rem'}}>
-                    <img className="card-img-top" src={model3} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
-                        <a href="#" className="btn btn-warning text-white">Buy</a>
-                    </div>
-                </div>
-                <div className="card m-2" style={{width: '18rem'}}>
-                <img className="card-img-top" src={model1} alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the
-                        bulk of the card's content.</p>
-                    <a href="#" className="btn btn-warning text-white">Buy</a>
-                </div>
-            </div>
-            </div>
-        </div>
-        <br /><br />
-
-        <div className="d-flex justify-content-center">
-            <button className='btn btn-outline-primary'>Load More </button>
-        </div>
 
     </div>
   );
--- src/shared/components/navigation/NavLinks.js
+++ src/shared/components/navigation/NavLinks.js
@@ -10,20 +10,30 @@ const NavLinks = props => {
     {id: 2, item: 'Men'},
     {id: 3, item: 'Hot'},
     {id: 4, item: 'Summer'},
+    {id: 4, item: 'Nights'},
   ];
 
   return (
     
     <ul className="navbar-nav mx-5 mr-auto">
-      <div class="list-group list-group-horizontal" role="group" aria-label="First group">
+      <div class="d-flex justify-content-center list-group list-group-horizontal" role="group" aria-label="First group">
         <button className="list-group-item mr-2 bg-transparent text-white" >New Arrivals</button>
         {categories.map((item, key) =>
             <button className="list-group-item mr-2 bg-transparent text-white">{item.item}</button>
         )
         }
-        {/*<button type="button" className="btn btn-secondary mr-2">New Arrivals</button>*/}
-        {/*<button type="button" className="btn btn-secondary mr-2">Products</button>*/}
-        {/*<button type="button" className="btn btn-secondary mr-2">T-shirts</button>*/}
+      </div>
+
+      <div className="dropdown">
+        <button className="list-group-item mr-2 bg-primary text-white dropdown-toggle" type="button" id="dropdownMenuButton"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          More Categories
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
       </div>
   </ul>
   );
