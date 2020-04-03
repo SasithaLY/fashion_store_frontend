4bd3302c5ab466bffda92df009a48acad750f349
Sasitha Layan Yasathilaka <onlinesasitha@gmail.com>
2020-04-03 09:13:05 +0530

    checkout half done

--- src/payment/pages/Checkout.js
+++ src/payment/pages/Checkout.js
@@ -15,9 +15,38 @@ const Checkout = () => {
                 <h6>Shipping Address</h6>
               </div>
               <div className="card-body">
-                    <div className="input-group">
-                        <input type="radio" className="custom-radio"></input>
-                    </div>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="gridRadios"
+                    id="gridRadios1"
+                    value="option1"
+                    checked
+                  />
+                  <label class="form-check-label" for="gridRadios1">
+                    Thilina Trade Center, Sandaresgama, Eppawala, Sri Lanka
+                  </label>
+                  <button className="btn btn-sm button-transparent mx-2"><i className="fas fa-pen icon-gold"></i></button>
+                </div>
+
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="gridRadios"
+                    id="gridRadios1"
+                    value="option1"
+                  />
+                  <label class="form-check-label" for="gridRadios1">
+                    148/7, Golden Cresent, Pittugala, Malabe, Sri Lanka
+                  </label>
+                  <button className="btn btn-sm button-transparent mx-2"><i className="fas fa-pen icon-gold"></i></button>
+                </div>
+                <div className="my-2">
+                    <button className="btn btn-sm btn-success">Add New</button>
+                </div>
+                
               </div>
             </div>
           </div>
