5ea8f540bc3c0280724db4e685b9a1ef743b1901
ishan <agishandissanayake@gmail.com>
2020-04-05 16:33:07 +0530

    Server updated.

--- Server/src/server.js
+++ Server/src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 // app.use(bodyParser.urlencoded({extended: true}));
