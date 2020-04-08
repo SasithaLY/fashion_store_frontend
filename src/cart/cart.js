653ba49fc61fabc51a4cd03b9f876c5e52539c46
kee <onlinekee@gmail.com>
2020-04-08 18:15:46 +0530

    cart added

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react'
+import moment from "moment";
+import {addItem} from './cartHelper'
+import { Link } from 'react-router-dom';
 
+const cart = ({product}) =>{
+    return(
+        <div className="col-4 mb-3">
+            <div className="card">
+                <div className="card-header">{product.name}</div>
+                <div className="card-body">
+                    <p>{product.description}</p>
+                    <p>${product.price}</p>
+                    <Link to="/">
+                        <button className="btn btn-outline-default mt-2 mb-2">
+                            
+                        </button>
+                    </Link>
+                </div>
+            </div>
+
+        </div>
+    )
+}
 export default class cart extends Component {
     render() {
         return (
--- src/cart/cartHelper.js
+++ src/cart/cartHelper.js
@@ -8,7 +8,13 @@ export const addItem = (item,next)=>{
         cart.push({
             ...item,
             count:1
-        })
+        });
+
+        cart = Array.from(new Set(cart.map((p)=>(p._id)))).map(id=>{
+            return cart.find(p._id === id)
+        });
+        localStorage.setItem("cart",JSON.stringify(cart));
+        next();
 
     }
 }
