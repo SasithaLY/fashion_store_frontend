89ce05f64013f1ccb6d133bbd37d2d67f6cfa69b
Sasitha Layan Yasathilaka <onlinesasitha@gmail.com>
2020-04-11 00:05:09 +0530

    changes to checkout

--- src/payment/components/Cart.js
+++ src/payment/components/Cart.js
@@ -1,63 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 
 import './Cart.css';
 
-const Cart = () => {
-  return (
-    <div className="custom-div">
-      <div className="order-md-2 mb-4">
-        <h4 className="d-flex justify-content-between align-items-center mb-3">
-          <span>Your cart</span>
-          <span className="badge badge-danger badge-pill">3</span>
-        </h4>
-        <ul className="list-group mb-3">
-          <li className="list-group-item d-flex justify-content-between lh-condensed">
-            <div>
-              <h6 className="my-0">Product name</h6>
-              <small className="custom-span">Brief description</small>
-            </div>
-            <span className="custom-span">$12</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between lh-condensed">
-            <div>
-              <h6 className="my-0">Second product</h6>
-              <small className="custom-span">Brief description</small>
-            </div>
-            <span className="custom-span">$8</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between lh-condensed">
-            <div>
-              <h6 className="my-0">Third item</h6>
-              <small className="custom-span">Brief description</small>
-            </div>
-            <span className="custom-span">$5</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between bg-dark">
-            <div className="text-success">
-              <h6 className="my-0">Promo code</h6>
-              <small>EXAMPLECODE</small>
-            </div>
-            <span className="text-success">-$5</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong>$20</strong>
-          </li>
-        </ul>
+export default class Cart extends Component {
+  render() {
+    const { cart, promocode, submitPromoCode, handleInputChange } = this.props;
+    let total = 0;
+    return (
+      <div className="custom-div">
+        <div className="order-md-2 mb-4">
+          <h4 className="d-flex justify-content-between align-items-center mb-3">
+            <span>Your cart</span>
+            <span className="badge badge-danger badge-pill">{cart.length}</span>
+          </h4>
+          <ul className="list-group mb-3">
+            {cart.map((item) => {
+              total += item.price * item.quantity;
+              return (
+                <li key={item.id} className="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 className="my-0">{item.itemName}</h6>
+                    <small className="custom-span">
+                      x {item.quantity}
+                    </small>
+                  </div>
+                  <span className="custom-span">
+                    LKR {(item.price * item.quantity).toFixed(2)}
+                  </span>
+                </li>
+              );
+            })}
+            {
+              promocode.applied ? <li className="list-group-item d-flex justify-content-between bg-dark">
+              <div className="text-success">
+                <h6 className="my-0">Promo code</h6>
+            <small>{promocode.code}</small>
+              </div>
+            <span className="text-success">- LKR {(total * promocode.discount / 100).toFixed(2)}</span>
+            </li> : null
+            }
+            
+            <li className="list-group-item d-flex justify-content-between">
+              <span>Total (LKR)</span>
+              <strong>{promocode.applied ? (total - (total * promocode.discount / 100)).toFixed(2) : total.toFixed(2)}</strong>
+            </li>
+          </ul>
 
-        <form className="card p-2">
-          <div className="input-group">
-            <input type="text" className="form-control" placeholder="Promo code" />
-            <div className="input-group-append">
-              <button type="submit" className="btn btn-success ">
-                Redeem
-              </button>
+          <form className="card p-2" onSubmit={submitPromoCode}>
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                name="promo"
+                placeholder="Promo code"
+                onChange={handleInputChange}
+              />
+              <div className="input-group-append">
+                <button type="submit" className="btn btn-success ">
+                  Redeem
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Cart;
+    );
+  }
+}
