913de140db0b1414799a1244f3a994fcd0204d85
ishan <agishandissanayake@gmail.com>
2020-05-03 22:32:43 +0530

    App.js updated!

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ import Home from "./home/pages/home";
 import Payment from './payment/pages/Payment';
 import Cart from "./cart/cart";
 import UploadProducts from "./Components/Upload Products/uploadProducts";
-import Login from "./user/Pages/Login";
-import Profile from "./user/Pages/Profile";
+import Login from "./user/pages/Login";
+import Profile from "./user/pages/Profile";
 import Checkout from "./payment/pages/Checkout";
 import SingleIProductDetails from "./Components/Products/singleIProductDetails";
-import Register from "./user/Pages/Register";
+import Register from "./user/pages/register";
 import Dashboard from "./user/adminPages/Dashboard"
 import UploadCategories from "./Components/UploadCategories/UploadCategories";
 import UploadProductsMultiple from "./Components/Upload Products/uploadProductsMultiple";
@@ -38,9 +38,8 @@ const App = () => {
       <div>
         <BrowserRouter>
 
-          <Header />
-
           <Switch>
+
             <Route path="/addProduct" exact>
               <UploadProducts />
             </Route>
@@ -50,42 +49,52 @@ const App = () => {
             </Route>
 
             <Route path="/" exact>
+              <Header />
               <Home />
             </Route>
 
             <Route path="/allProducts/:categoryId" exact>
+              <Header />
               <AllProductsByCategory />
             </Route>
 
             <Route path="/products/:id" exact>
+              <Header />
               <SingleIProductDetails />
             </Route>
 
             <Route path="/cart" exact>
-              <Cart />
+              <Header />
+              <Cart/>
             </Route>
 
             <Route path="/order" exact>
+              <Header />
               <h2>Add order page here</h2>
             </Route>
 
             <Route path="/checkout" exact>
+              <Header />
               <Checkout />
             </Route>
 
             <Route path="/contact" exact>
+              <Header />
               <h2>Add contact page here</h2>
             </Route>
 
             <Route path="/about" exact>
+              <Header />
               <h2>Add about page here</h2>
             </Route>
 
             <Route path="/signin" exact>
+              <Header />
               <Login></Login>
             </Route>
 
             <Route path="/signup" exact>
+              <Header />
               <Register></Register>
             </Route>
 
@@ -102,6 +111,7 @@ const App = () => {
             </Route>
 
             <Route path="/allProducts" exact>
+              <Header />
               <AllProducts />
             </Route>
 
