bad82f824293c85c38ecf62775779a54d3202cfe
it17163132@my.sliit.lk <it17163132@my.sliit.lk>
2020-05-20 01:04:17 +0530

    view all update

--- src/user/Admin/ViewUsers.js
+++ src/user/Admin/ViewUsers.js
@@ -1,6 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from 'react-router-dom';
+import { isAuthenticated } from "../../auth/auth";
+import { listUsers } from "../UserAPIs/userApi";
 
 const ViewUsers = () => {
+
+    const [users, setUsers] = useState([])
+
+    const{user, token} = isAuthenticated()
+
+    const loadUsers = () => {
+        listUsers(user._id, token).then(data => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                setUsers(data)
+            }
+        })
+    }
+
+    useEffect(() => {
+        loadUsers()
+    }, [])
 
     const userTable = () => (
         <div className="container-fluid">
@@ -13,6 +34,7 @@ const ViewUsers = () => {
     return (
         <div>
             {userTable()}
+            {JSON.stringify(users)}
         </div>        
     )
 }
