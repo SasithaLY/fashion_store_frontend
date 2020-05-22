0bfcd565998bb3247688799cb3a24f603fab4943
it17163132@my.sliit.lk <it17163132@my.sliit.lk>
2020-05-22 11:56:03 +0530

    dashboard view update

--- src/user/Admin/Dashboard.js
+++ src/user/Admin/Dashboard.js
@@ -1,50 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { isAuthenticated } from "../../auth/auth";
-import userImg from "../../shared/assets/images/index.png"
 
 const userProfile = () => {
 
     const { user: { _id, fName, lName, email, gender, role } } = isAuthenticated();
 
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 img">
-                        <h3> Hi! {fName}, Have a Good Day! </h3> <br />
-                        <img
-                            src={userImg}
-                            alt=""
-                            className="img-rounded"
-                        />
-                    </div>
-                    <div className="col-md-6 details">
-                        <blockquote>
-                            <h5>{fName} {lName}</h5>
-                        </blockquote>
-                        <p> Email: {email}  </p>
-                        <p> Gender: {gender}  </p>
-                        <p> Role: {role === 1 ? "Admin" : role === 2 ? "Store Manager" : "Registered User"}  </p>
-                        <p>
-                            <Link className="nav-link" aria-selected="true" to={`/user/editProfile/${_id}`}>
-                                Edit Profile
-                            </Link>
-                        </p>
-                    </div>
-                </div>
+        <div className="card text-center">
+            <div className="card-header">
+                <h2>Profile</h2>
             </div>
+            <div className="card-body">
+                <h5 className="card-title">Hi! {fName}, Have a Good Day! </h5>
+                <p className="card-text">Name: {fName} {lName}</p>
+                <p className="card-text">Email: {email}</p>
+                <p className="card-text">Gender: {gender}</p>
+                <p className="card-text">Role: {role === 1 ? "Admin" : role === 2 ? "Store Manager" : "Registered User"} </p>
+                <p>
+                    <Link classNameName="nav-link" aria-selected="true" to={`/user/editProfile/${_id}`}>
+                        Edit Profile
+                </Link>
+                </p>
+            </div>
+            <div className="card-footer text-muted">
+                Fashion Store
+      </div>
         </div>
     )
 }
 
 const Dashboard = () => {
     return (
-        <div className="row">
-            <div className="col-3">
-
-            </div>
-            <div className="col-9">
+        <div>
+            <div>
                 {userProfile()}
             </div>
         </div>
--- src/user/pages/Profile.js
+++ src/user/pages/Profile.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { isAuthenticated } from "../../auth/auth";
-import userImg from "../../shared/assets/images/index.png"
 import UserNavs from "../components/UserNavs"
 
 const userProfile = () => {
@@ -9,28 +8,10 @@ const userProfile = () => {
   const { user: { _id, fName, lName, email, gender, role } } = isAuthenticated();
 
   return (
-    // <div>
-    //   <div classNameName="container-fluid">
-
-    //     <h3> Hi! {fName}, Have a Good Day! </h3> <br />
-    //     <div>
-    //       <h5>{fName} {lName}</h5>
-    //       <p> Email: {email}  </p>
-    //       <p> Gender: {gender}  </p>
-    //       <p> Role: {role === 1 ? "Admin" : role === 2 ? "Store Manager" : "Registered User"}  </p>
-    //       <p>
-    //         <Link classNameName="nav-link" aria-selected="true" to={`/user/editProfile/${_id}`}>
-    //           Edit Profile
-    //           </Link>
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="card text-center">
       <div className="card-header">
         <h2>Profile</h2>
-    </div>
+      </div>
       <div className="card-body">
         <h5 className="card-title">Hi! {fName}, Have a Good Day! </h5>
         <p className="card-text">Name: {fName} {lName}</p>
@@ -47,14 +28,14 @@ const userProfile = () => {
         Fashion Store
     </div>
     </div>
- )
+  )
 }
 
 const Profile = () => {
 
   return (
 
-    <div classNameName="row">
+    <div>
       <UserNavs />
       <div>
         {userProfile()}
