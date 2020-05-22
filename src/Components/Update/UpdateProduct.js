fa878e9cead680393685563fc2e37701f8ad8219
ishan <agishandissanayake@gmail.com>
2020-05-22 12:55:51 +0530

    Changes in Products.

--- src/Components/Update/UpdateProduct.js
+++ src/Components/Update/UpdateProduct.js
@@ -200,10 +200,9 @@ const UpdateProduct = ({match}) => {
 
                             <div className="form-group">
                                 <label className="text-warning">Category</label>
-                                <select onChange={handleChange('category')} className="form-control">
-                                    <option>Please select</option>
+                                <select onChange={handleChange('category')} className="form-control" value={category}>
                                     {categories && categories.map((c, i) => (
-                                        <option key={i} value={c._id} selected={category === c._id}>
+                                        <option key={i} value={c._id}>
                                             {c.categoryName}
                                         </option>
                                     ))}
@@ -212,9 +211,9 @@ const UpdateProduct = ({match}) => {
 
                             <div className="form-group">
                                 <label className="text-warning">Shipping</label>
-                                <select onChange={handleChange('shipping')} className="form-control">
-                                    <option value="1" selected={shipping === true} >Yes</option>
-                                    <option value="0" selected={shipping === false}>No</option>
+                                <select onChange={handleChange('shipping')} value={shipping} className="form-control">
+                                    <option value='true' >Yes</option>
+                                    <option value='false' >No</option>
                                 </select>
                             </div>
 
